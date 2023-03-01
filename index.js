414d9d5e84acf0352b274ed28abadea6dd7c2985
boy-son <branden.wheat@yahoo.com>
2023-03-01 13:09:25 -0500

    added to index.js, used placeholders for output directory and outputPath for now

--- index.js
+++ index.js
@@ -38,5 +38,91 @@ inquirer.prompt([
 })
 }
 
-function addTeamMember() {}
+function addTeamMember() {
+inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'role',
+        message: "What is the role of the team member?",
+        choices: ['Engineer', 'Intern', 'I do not want to add any more team members']
+    },
+])
+.then((answers) => {
+switch (answers.role) {
+    case 'Engineer': 
+    addEngineer();
+    break;
+    case 'Intern':
+        addIntern();
+        break;
+        default:
+            buildTeam();
+}    
+})   
+}
+}
+
+function addEngineer() {
+    inquirer.prompt ([{
+        type: 'input',
+        name: 'name',
+        message: "What is the name of the engineer?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the engineer's id?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the engineer's email?",
+
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the engineer's github username?",
+    },
+])
+.then((answers) => {
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    team.push(engineer);
+    addTeamMember();
+})
+}
+
+function addIntern() {
+    inquirer.prompt ([{
+        type: 'input',
+        name: 'name',
+        message: "What is the name of the intern?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the intern's id?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the intern's email?",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What is the intern's school?",
+    },
+])
+.then((answers) => {
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    team.push(intern);
+    addTeamMember();
+})
+}
+
+function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR))
+    fs.mkdirSync(OUTPUT_DIR);
+    fs.writeFileSync(outputPath, render(team), "utf-8");
 }
